30c96115242340334c8f4d3d5980e15c4a86ef58
Lumbha Ram <63182965+erlumbharam@users.noreply.github.com>
2021-02-12 14:06:29 +0530

    Create 2017.C

---
+++ 2017.C
@@ -0,0 +1,25 @@
+//Consider the following C program:
+
+#include <stdio.h>
+
+int jumble(int x, int y){
+
+x=2*x+y;
+
+return x;
+
+}
+
+int main(){ 
+
+int x=2, y=5;
+
+y=jumble(y,x);
+
+x=jumble(y,x);
+
+printf(“%d \n”, x);
+
+return 0;
+
+}
